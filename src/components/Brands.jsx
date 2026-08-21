2c5ac432e181f6c21ae925d35ee6002812c3cbe0
Motanis-Homes <motanishomes@gmail.com>
2026-08-21 17:13:31 +0100

    <TEST++: Same Brands.jsx test as previous - w=400 Thumbnail sizing++>

--- src/components/Brands.jsx
+++ src/components/Brands.jsx
@@ -3,7 +3,7 @@ import React, { useEffect, useRef, useState } from 'react';
 const brands = [
   {
     name: 'Ferrari',
-    logo: 'https://drive.google.com/thumbnail?id=1sQfgcSOcN4_OocU-CpCPcrNjrqHmPMqD=w400',
+    logo: 'https://drive.google.com/thumbnail?id=1sQfgcSOcN4_OocU-CpCPcrNjrqHmPMqD',
   },
   {
     name: 'BMW',
